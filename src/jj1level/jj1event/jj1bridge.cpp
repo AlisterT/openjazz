591f673537af73e663df85a7e83b26c8b1bfa2db
Carsten Teibes <dev@f4ke.de>
2019-01-12 19:02:17 +0100

    Update License information

--- src/jj1level/jj1event/jj1bridge.cpp
+++ src/jj1level/jj1event/jj1bridge.cpp
@@ -20,10 +20,6 @@
  * OpenJazz is distributed under the terms of
  * the GNU General Public License, version 2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
  * @par Description:
  * Provides the functions of bridge events.
  *
