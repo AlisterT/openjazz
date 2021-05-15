2e99a60cd5bae18f33d79fbb1b41de5cbbd01670
Carsten Teibes <dev@f4ke.de>
2021-05-15 16:56:33 +0200

    3DS: Use swkbd applet for user input Fix IP input to not allow letters and spaces

--- src/platforms/3ds.cpp
+++ src/platforms/3ds.cpp
@@ -12,6 +12,7 @@
  */
 
 #include "3ds.h"
+#include "util.h"
 
 #ifdef _3DS
 
@@ -29,6 +30,69 @@ void N3DS_SetKeyMap() {
 	SDL_N3DSKeyBind(KEY_DDOWN, SDLK_DOWN);
 	SDL_N3DSKeyBind(KEY_DLEFT, SDLK_LEFT);
 	SDL_N3DSKeyBind(KEY_DRIGHT, SDLK_RIGHT);
+
+}
+
+int N3DS_InputIP(char*& current_ip, char*& new_ip) {
+
+	SwkbdState swkbd;
+	char ip_buf[16];
+	SwkbdButton button = SWKBD_BUTTON_NONE;
+
+	swkbdInit(&swkbd, SWKBD_TYPE_NUMPAD, 2, 15);
+	swkbdSetInitialText(&swkbd, current_ip);
+	swkbdSetHintText(&swkbd, "Please enter an IP address");
+	swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
+	swkbdSetFeatures(&swkbd, SWKBD_FIXED_WIDTH);
+	swkbdSetNumpadKeys(&swkbd, '.', 0);
+
+	button = swkbdInputText(&swkbd, ip_buf, sizeof(ip_buf));
+	if (button == SWKBD_BUTTON_CONFIRM) {
+		new_ip = createString(ip_buf);
+		return 1;
+	}
+
+	return 0;
+
+}
+
+static SwkbdCallbackResult StringFilter(void*, const char** ppMessage, const char* text, size_t textlen) {
+
+	for (size_t i = 0; i < textlen; i++) {
+
+		if ((text[i] != ' ') && (text[i] != '.') &&
+			((text[i] < '0') || (text[i] > '9')) &&
+			((text[i] < 'a') || (text[i] > 'z'))) {
+
+			*ppMessage = "Invalid character(s) found.\nOnly a-z, 0-9, Dot and Space\nare allowed.";
+			return SWKBD_CALLBACK_CONTINUE;
+
+		}
+
+	}
+
+	return SWKBD_CALLBACK_OK;
+}
+
+int N3DS_InputString(const char* hint, char*& current_string, char*& new_string) {
+
+	SwkbdState swkbd;
+	char string_buf[STRING_LENGTH + 1];
+	SwkbdButton button = SWKBD_BUTTON_NONE;
+
+	swkbdInit(&swkbd, SWKBD_TYPE_QWERTY, 2, STRING_LENGTH);
+	swkbdSetInitialText(&swkbd, current_string);
+	swkbdSetHintText(&swkbd, hint);
+	swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
+	swkbdSetFilterCallback(&swkbd, StringFilter, NULL);
+
+	button = swkbdInputText(&swkbd, string_buf, sizeof(string_buf));
+	if (button == SWKBD_BUTTON_CONFIRM) {
+		new_string = createString(string_buf);
+		return 1;
+	}
+
+	return 0;
 
 }
 
