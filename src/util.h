3e66a32a5dd29a2afd3d88999419c1cacfcee9e4
Carsten Teibes <dev@f4ke.de>
2024-03-07 00:45:02 +0100

    Use STL for PathMgr/Path Simplify

--- src/util.h
+++ src/util.h
@@ -33,7 +33,7 @@ EXTERN fixed sinLut[1024];
 
 // Functions
 
-EXTERN bool               fileExists           (const char *fileName, int pathType = PATH_TYPE_ANY);
+EXTERN bool               fileExists           (const char *fileName, int pathType);
 EXTERN unsigned short int createShort          (const unsigned char* data);
 EXTERN int                createInt            (const unsigned char* data);
 EXTERN char*              createString         (const char *string);
