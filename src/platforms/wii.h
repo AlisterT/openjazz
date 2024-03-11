2fca670f1ebb0936543227c41e346e00bda942f6
Carsten Teibes <dev@f4ke.de>
2024-03-12 00:58:54 +0100

    Refactor: Move platform config in specific headers Build Wii port with new SDL2 port, todo: PSP, PSVita, Android

--- src/platforms/wii.h
+++ src/platforms/wii.h
@@ -23,6 +23,26 @@ void WII_Init();
 
 void WII_AddGamePaths();
 
+// video config (SDL2)
+
+//#define DEFAULT_SCREEN_WIDTH 640 // FIXME: enable after scaling is available
+//#define DEFAULT_SCREEN_HEIGHT 480
+#define DEFAULT_SCREEN_WIDTH 320
+#define DEFAULT_SCREEN_HEIGHT 240
+#define FULLSCREEN_ONLY
+#define NO_RESIZE
+
+// controller config (SDL2)
+
+#define DEFAULT_BUTTON_JUMP   (2) /* 1 */
+#define DEFAULT_BUTTON_SWIM   (2) /* 1 */
+#define DEFAULT_BUTTON_FIRE   (3) /* 2 */
+#define DEFAULT_BUTTON_CHANGE (0) /* A */
+#define DEFAULT_BUTTON_ENTER  (3) /* 2 */
+#define DEFAULT_BUTTON_ESCAPE (6) /* Home */
+#define DEFAULT_BUTTON_STATS  (4) /* - */
+#define DEFAULT_BUTTON_PAUSE  (5) /* + */
+
 #endif
 
 #endif
